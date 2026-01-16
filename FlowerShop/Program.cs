66caca7db6d019fe48305d4e10266c9a2090f073
HathalerMichael <m.hathaler@students.htl-leonding.ac.at>
2026-01-16 12:36:11 +0100

    Update .gitignore

--- FlowerShop/Program.cs
+++ FlowerShop/Program.cs
@@ -4,3 +4,6 @@ var app = builder.Build();
 app.MapGet("/ping", () => "pong");
 
 app.Run();
+
+// Make the implicit Program class public so test projects can access it
+public partial class Program { }
